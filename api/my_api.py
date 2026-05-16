361cd56f89fcf8970868a3b70b68508269cb2d8e
Wellington Roveder <rovederwellington@gmail.com>
2026-05-16 14:19:06 -0300

    fix HTTPException 500 instead of return 200 with error

--- api/my_api.py
+++ api/my_api.py
@@ -1,20 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 from services.sentinel_price import executar_monitor 
 from utils.logger import configurar_logger
+import os
+
+logger = configurar_logger()
 
 app = FastAPI()
+
+API_KEY = os.getenv("INTERNAL_API_KEY")
+
 
 @app.get("/")
 def home():
     return {"status": "API rodando"}
 
 @app.post("/executar")
-def rodar_script():
-    logger = configurar_logger()
+def rodar_script(x_api_key: str = Header()):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Não autorizado")
     try:
         jogos_promocao = executar_monitor() 
         logger.info("Sucesso")
         return {"status": "ok", "Promoçoes": jogos_promocao}
     except Exception as e:
         logger.error(f"erro: {e}")
-        return {"status": "erro", "detail": str(e)}
+        raise HTTPException(status_code=500, detail="Erro interno no Servidor")
