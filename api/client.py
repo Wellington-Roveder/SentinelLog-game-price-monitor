a8eb94b8f4e5dba077779d8fafb183a2dcc23eb1
Wellington Roveder <rovederwellington@gmail.com>
2026-05-20 21:57:29 -0300

    feat automatic retry for gameapi

--- api/client.py
+++ api/client.py
@@ -1,14 +1,52 @@
 import requests
+import time
+from requests.exceptions import RequestException,HTTPError
+import functools
 from utils.logger import configurar_logger
 
 logger = configurar_logger()
+
+def retry(max_retries=3,backoff_factor=2,status_forcelist=(500,502,504)):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args ,**kwargs):
+            retries = 0
+            while retries < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                
+                    
+                except (RequestException, ConnectionError) as e:
+                    retries += 1
+                    logger.warning(f"Tentando novamente, Tentativa: {retries} Motivo {e}")
+
+
+                    if isinstance(e, HTTPError) and e.response.status_code not in status_forcelist:
+                        logger.error(f"Erro na Execuçao {e}")
+                        raise e
+                        
+                        
+                        
+                    if retries >= max_retries:
+                        logger.error("Tentativas Excedeu os Limites")
+                        raise e
+                        
+
+                    sleep_time = backoff_factor ** retries
+                    time.sleep(sleep_time)
+        return wrapper
+    return decorator
+
+
+
+
 
 class APIClient:
     def __init__(self, base_url):
        self.base_url = base_url
        self.session = requests.Session() # reutiliza conexao http mais rapido menos consumo
 
-
+    @retry(max_retries=3, backoff_factor=2)
     def get(self, endpoint):
         try:
             response = self.session.get(f"{self.base_url}/{endpoint}", timeout= 10) #reutiliza sessao, timeout para nao dar trava para sempre
@@ -18,5 +56,7 @@ class APIClient:
            
         except requests.exceptions.RequestException as e:
             logger.error(f"Erro na Api: {e}") # mostra mensagem de erro 404,500
-            return None
+            raise e
        
+
+    
--- api/game_api_client.py
+++ api/game_api_client.py
@@ -1,19 +1,21 @@
 from api.client import APIClient
 from utils.logger import configurar_logger
 
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
+logger = configurar_logger()
 
 class GameAPI:
     def __init__(self):
         self.api = APIClient(base_url=os.getenv('CHEAPSHARK_URL'))
-        self.logger = configurar_logger()
         self.loja = []
 
+    
     def buscar_nome_jogo(self, nome_jogo):
+        
         endpoint = f"games?title={nome_jogo}"
         dados = self.api.get(endpoint)
 
@@ -27,9 +29,9 @@ class GameAPI:
                 }
             return None
         except Exception as e:
-            self.logger.error(f"Erro ao buscar jogo: {e}")
-            return None
-
+            logger.error(f"Erro ao buscar jogo: {e}")
+            raise e
+    
     def buscar_loja(self, deal_id):
         endpoint = f"deals?id={deal_id}"
         dados = self.api.get(endpoint)
@@ -40,9 +42,9 @@ class GameAPI:
                 return self.get_nome_loja(store_id)
             return None
         except Exception as e:
-            self.logger.error(f"Erro ao buscar loja: {e}")
-            return None
-
+            logger.error(f"Erro ao buscar loja: {e}")
+            raise e
+    
     def get_nome_loja(self, store_id):
         endpoint = "stores"
 
