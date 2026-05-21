1126c5c278ce075045338d266eea8b93f4925a3b
Wellington Roveder <rovederwellington@gmail.com>
2026-05-21 01:15:55 -0300

    feat: redis implementation

--- api/game_api_client.py
+++ api/game_api_client.py
@@ -1,6 +1,7 @@
 from api.client import APIClient
 from utils.logger import configurar_logger
-
+from database.redis_client import r
+import json
 
 from dotenv import load_dotenv
 import os
@@ -12,6 +13,7 @@ class GameAPI:
     def __init__(self):
         self.api = APIClient(base_url=os.getenv('CHEAPSHARK_URL'))
         self.loja = []
+        self.redis_falhou = False
 
     
     def buscar_nome_jogo(self, nome_jogo):
@@ -47,12 +49,44 @@ class GameAPI:
     
     def get_nome_loja(self, store_id):
         endpoint = "stores"
+        
+        if self.redis_falhou:
+                return None 
+        try:
+            cache_key = "cheapshark:stores"
+            stores_cache = r.get(cache_key)
+            if stores_cache:
+                stores = json.loads(stores_cache)
+                for loja in stores:
+                    if loja['storeID'] == store_id:
+                        return loja['storeName']
+                return "Desconhecida"    
+            else:
 
-        if not self.loja:
-            self.loja = self.api.get(endpoint)
+                stores = self.loja = self.api.get(endpoint)
+                r.setex(
+                    cache_key,
+                    60 * 60 * 24,
+                    json.dumps(stores)
+                )
+                for loja in stores:
+                    if loja['storeID'] == store_id:
+                        return loja['storeName']
+                return "Desconhecida"
+        except Exception as e:
+            endpoint = "stores"
+            self.redis_falhou = True
+            logger.error(f"Erro no redis {e}")
 
-        for loja in self.loja:
-            if loja['storeID'] == store_id:
-                return loja['storeName']
+            if not self.loja:
+                self.loja = self.api.get(endpoint)
+                
 
-        return "Desconhecida"
+            for loja in self.loja:
+                if loja['storeID'] == store_id:
+                    return loja['storeName']
+                
+            return "Desconhecida"
+            
+            
+        
---
+++ database/redis_client.py
@@ -0,0 +1,8 @@
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+r = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), decode_responses=True, socket_connect_timeout=5,socket_timeout=3)
